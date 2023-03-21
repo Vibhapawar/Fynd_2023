5e9d5601486352e906d5513e24d3c297ac924f96
Vibhapawar <83806838+Vibhapawar@users.noreply.github.com>
2023-03-21 12:30:08 +0530

    commit

---
+++ MEVN/Vuejs/vue-project/src/components/question2_architectuure.vue
@@ -0,0 +1,13 @@
+MVVM architecture:
+MMVM stands for Model View View-Model,
+MMVM is a pattern that separates the graphical UI from logic. 
+
+View: 
+View is the actual data object model . It uses DOM. View layer is responsible for displaying data.
+
+View-Model:
+It is an object that syncs the model and view. Every vue instance is a view model. 
+It consists of DOM Listener and Data Binding. 
+
+Model:
+Model layer is responsible for storing of data.
